4ebff13a4720464977f2a6dda11ae2e067c8266f
Your Name <new2282307660@gmail.com>
2021-03-07 10:00:53 -0800

    detail page exit button

--- web/src/Details.js
+++ web/src/Details.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components"
+import ExitToAppIcon from '@material-ui/icons/ExitToApp'
 
 export default class Details extends React.Component {
     constructor(props) {
@@ -32,9 +33,23 @@ export default class Details extends React.Component {
             <PersonContainer>
                 <NameContainer>
                 <p>{achievement.name}</p>
+                <a href="/"><Exit href="/"/></a>                
                 </NameContainer>
                 <PictureContainer>
-                    <img src={achievement.image_url} alt=""/>
+                    <img src={achievement.image_url} alt=""/>     
+                    <InfoContainer>
+                        <h1>{achievement.category}</h1>
+                        <LinkContainer>
+                            {
+                                achievement.links.map(link => (
+                                    <LinkButton href={link} target="_blank">
+                                        Click Here
+                                    </LinkButton> 
+                                ))
+                            }
+                        </LinkContainer>
+                        
+                    </InfoContainer>                                   
                 </PictureContainer>
                 <DetailsContainer>
                     <p>{achievement.description}</p>
@@ -65,7 +80,7 @@ const PromptContainer = styled.div`
 const PersonContainer = styled.div`
     background: #99d5ca;
     display: grid;
-    grid-template-rows: 100px 400px auto;
+    grid-template-rows: 100px 300px auto;
 
 `
 
@@ -85,14 +100,18 @@ const NameContainer= styled.div`
 const PictureContainer = styled.div`
     padding-top: 50px;
     padding-bottom: 50px;
-    padding-left:150px;
+    margin-left:300px;
+    margin-right:300px;
     display: flex;
-    justify-content: left;
+    justify-content: center;
     align-items: center;
+    
     img {
         border: 6px solid  #f50057;
+        box-shadow: 0 1px 3px rgb(0 0 0 /12%), 0 1px 2px rgb( 0 0 0 /24%);
         border-radius: 10px;  
-        width:20%;     
+        width:30%;   
+        margin-right: 50px;  
     }
 `
 
@@ -104,4 +123,62 @@ const DetailsContainer = styled.div`
     padding-left: 50px;
     padding-right: 50px;
     border-radius: 10px;
+    box-shadow: 0 1px 3px rgb(0 0 0 /12%), 0 1px 2px rgb( 0 0 0 /24%);
+
+`
+
+const InfoContainer = styled.div`
+    display:flex;
+    flex-direction: column;
+    justify-content: center;
+    margin-left:100px;
+    h1 {
+        color: white;
+        box-shadow: 0 1px 3px rgb(0 0 0 /12%), 0 1px 2px rgb( 0 0 0 /24%);
+        padding-left:10px;
+        padding-right:10px;
+        border-radius: 6px;
+        display:flex;
+        justify-content: center;
+
+    }
+
+`
+
+const LinkButton = styled.a`
+    margin-top: 50px;
+    margin-left: 30px;
+    padding-left: 10px;
+    padding-right: 10px;
+    background-color: white;
+    border: none;
+    height: 40px;
+    border-radius: 4px;
+    cursor: pointer;
+    font-size: 15px;
+    text-decoration: none;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: black;
+    box-shadow: 0 2px 6px rgb(0 0 0 /12%), 0 2px 6px rgb( 0 0 0 /24%);
+    :hover {
+        background: #ffebee;
+    }
+
+`
+
+const LinkContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`
+
+const Exit = styled(ExitToAppIcon)`
+    margin-left: 10px;
+    display: flex;
+    align-items: center; 
+    background: white;
+    border-radius: 6px;
+    cursor: pointer;
 `
